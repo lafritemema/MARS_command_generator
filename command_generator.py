bbab34fe3bac628b41dee26a0b73cc302c4dad7e
nz84200 <florent.dubois@airbus.com>
2022-06-13 15:46:52 +0200

    implement http server

--- command_generator.py
+++ command_generator.py
@@ -1,29 +1,53 @@
-import json
-from server.exception import ServerException
-from utils import get_config_from_file
+from server.exceptions import ServerException
+from utils import get_config_from_file, get_validation_schemas
+import sys
 import os
 from dotenv import load_dotenv
-from typing import Dict, List
+from typing import Dict, Tuple
 from server.amqp import AMQPServer, CFunction, CPipeline
-from exception import BaseException, BaseExceptionType
-from server.exception import ServerException
-import sys
+from server.http import HttpServer, EFunction
+from server.validation import Validator
+from exceptions import BaseException, BaseExceptionType
+from server.exceptions import ServerException
 import logging
 
 import model
 from model.action import Action
 import mars
 
+# TODO update server code define in build_processor
+
 load_dotenv()
+
+# get server config file
 __SERVER_CONFIG_FILE = os.getenv('SERVER_CONFIG')
 __SERVER_CONFIG_FILE = __SERVER_CONFIG_FILE if __SERVER_CONFIG_FILE else './config/server.yaml'
+
+# get mars config file
 __MARS_CONFIG_FILE = os.getenv('MARS_CONFIG')
 __MARS_CONFIG_FILE = __MARS_CONFIG_FILE if __MARS_CONFIG_FILE else './config/mars.yaml' 
+
+# get validation schema directory
+__VALIDATION_SCHEMA_DIR = os.getenv('VALIDATION_SCHEMA_DIR')
+__VALIDATION_SCHEMA_DIR = __VALIDATION_SCHEMA_DIR if __VALIDATION_SCHEMA_DIR else './schemas'
+
+# declare amqp topics
 __AMQP_TOPICS = 'request.command_generator','report.command_generator'
 
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOGGER = logging.getLogger("cmd_generator")
 logging.getLogger("pika").setLevel(logging.WARNING)
+
+def get_http_para_from_config(http_config:Dict) -> Tuple[str, int]:
+  try:
+    host = http_config['host']
+    port = http_config['port']
+    return host, port
+  except KeyError as error:
+    raise BaseException(["SERVER", "HTTP"],
+                        BaseExceptionType.CONFIG_NOT_CONFORM,
+                        f"http server config is not conform, {error.args[0]} parameter is missing")
+
 
 def build_amqp_server(amqp_config:Dict):
   try:
@@ -37,8 +61,7 @@ def build_amqp_server(amqp_config:Dict):
                             host=host,
                             port=port,
                             exchange_name=ex_name,
-                            exchange_type=ex_type)
-                            
+                            exchange_type=ex_type)                  
     return amqp_server
 
   except KeyError as error:
@@ -51,64 +74,100 @@ def build_amqp_server(amqp_config:Dict):
     error.add_in_stack(['SERVER'])
     raise error
 
-def build_commands(body:Dict, headers:Dict):
-  LOGGER.info()
-  id = body.get('id')
-  action = Action.get_from_db(id)
-  return action.get_commands(), headers
 
+def build_commands(body:Dict,
+                   headers:Dict,
+                   path:str,
+                   query_args:Dict):
+  #get the action id from the body
+  id = body.get('id')
+  # extract data from database and generate an action
+  action = Action.get_from_db(id)
+  # build a body with commands
+  body = {
+    "commands":action.get_commands()
+  }
+  
+  return body, headers
+
+def build_validator(schemas_dict:Dict)-> Validator:
+  # instanciate validator to validate request 
+  validator = Validator()
+  # add all schemas in the validation object
+  for path, schema in schemas_dict.items():
+    validator.add_schema(path, schema)
+  
+  return validator
 
 def main():
-  amqp_server = None
-  http_server = None
+
+  AMQP_SERVER:AMQPServer = None
+  HTTP_SERVER:HttpServer = None
   
+  # read the configurations
   LOGGER.info("read configurations")
   SERVER_CONFIG = get_config_from_file(__SERVER_CONFIG_FILE)
   MARS_CONFIG = get_config_from_file(__MARS_CONFIG_FILE)
+  
+  # get server configurations
   amqp_config:Dict = SERVER_CONFIG.get('amqp')
   http_config:Dict = SERVER_CONFIG.get('http')
 
+  # get validations schemas stored in __VALIDATION_SCHEMA_DIR
+  schemas_dict = get_validation_schemas(__VALIDATION_SCHEMA_DIR)
+  # build the validator object
+  request_validator = build_validator(schemas_dict)
+
+  # load mars environment
   LOGGER.info("load mars environment")
   model.EQUIPMENT, model.REFERENCE, model.COMMAND_REGISTER, model.DB_DRIVER = mars.build_environment(MARS_CONFIG)
+
 
   # if amqp server configuration is defined and if parameter activate == true
   if amqp_config and amqp_config.get('activate'):
     LOGGER.info("build amqp server")
-    amqp_server = build_amqp_server(amqp_config)
+    AMQP_SERVER = build_amqp_server(amqp_config)
 
     LOGGER.info('configure amqp server')
-    amqp_server.add_queue(label="request_report",
+    AMQP_SERVER.add_queue(label="request_report",
                           topics=__AMQP_TOPICS)
     
 
     # prepare a consumer pipeline
     # no topic parameter for publish => report_topic contained in the message header
     req_pipeline = CPipeline([CFunction(build_commands),
-                              CFunction(amqp_server.publish)])
+                              CFunction(AMQP_SERVER.publish)])
     
-    amqp_server.add_consumer('request.command_generator', req_pipeline)
+    AMQP_SERVER.add_consumer('request.command_generator', req_pipeline)
   
   if http_config and http_config.get('activate'):
     # http server configuration - not implemented yet
-    # TODO implement http_server and run 
-    pass
+    # TODO implement http_server and run
+    LOGGER.info("build http server")
+    HTTP_HOST, HTTP_PORT = get_http_para_from_config(http_config)
+    HTTP_SERVER = HttpServer("command_generator", request_validator)
 
-  if not http_server and not amqp_server:
+    LOGGER.info("configure http server")
+    HTTP_SERVER.add_endpoint('/commands/generate',
+                             'approach',
+                             EFunction(build_commands),
+                             methods=['GET'])
+
+  if not HTTP_SERVER and not AMQP_SERVER:
     raise BaseException(['CONFIG', 'SERVER'],
                         BaseExceptionType.CONFIG_NOT_CONFORM,
                         "no server activated, check the configuration")
   
-  if http_server:
+  if HTTP_SERVER:
+    LOGGER.info('run http server and wait for messages')
     # http_server run on new tread - not implemented yet
-    pass
+    HTTP_SERVER.run(HTTP_HOST, HTTP_PORT)
 
-  if amqp_server:
+  if AMQP_SERVER:
     # run amqp server on the current tread
     LOGGER.info('run amqp server and wait for messages')
-    amqp_server.run()
+    AMQP_SERVER.run()
   
-
-
 if __name__ == '__main__':
   try:
     LOGGER.info("run command_generator service")
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from enum import EnumMeta
 from pyrsistent import b
 import yaml
-from exception import BaseException, BaseExceptionType
+import json
+import glob
+from exceptions import BaseException, BaseExceptionType
 from typing import Dict
 
 # define metaclass for enumeration access
@@ -15,6 +17,49 @@ class GetAttrEnum(EnumMeta):
 class GetItemEnum(EnumMeta):
   def __getitem__(self, name):
     return super().__getitem__(name).value
+
+def get_validation_schemas(schemas_dir:str)->Dict[str, Dict]:
+  """function to read and parse all json schemas contained in a directory
+    the schema files must have a .schema.json extension
+
+  Args:
+      schemas_dir (str): path of directory
+
+  Raises:
+      BaseException: raise if json format is not conform or elements are missing
+
+  Returns:
+      Dict: the configuration under dict format
+  """
+
+  schema_dict = {}
+
+  try:
+    for file in glob.glob(f"{schemas_dir}/*.schema.json"):
+      with open(file, 'r') as schema_file:
+        content = schema_file.read()
+        schema = json.loads(content)
+
+        # get the paths to validate with schema
+        # schema must contain a $paths key containing the paths to validate with the schema
+        # if not raise a config error
+        path_list = schema['$paths']
+                
+        # add the schema for each path
+        for path in path_list:
+          schema_dict[path] = schema
+        
+      return schema_dict
+  except json.JSONDecodeError as error :
+    raise BaseException(["CONFIG", "VALIDATION_SCHEMA"],
+                        BaseExceptionType.CONFIG_NOT_CONFORM,
+                        f"Validation schema file {file} is not valid, json format not conform\n{error.args[0]}")
+  except KeyError as error:
+    raise BaseException(["CONFIG", "VALIDATION_SCHEMA"],
+                        BaseExceptionType.CONFIG_NOT_CONFORM,
+                        f"Validation schema file {file} is not valid, key '$paths' is missing")
+
+
 
 def get_config_from_file(yaml_file:str)-> Dict:
     try:
